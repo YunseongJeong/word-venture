e6d34098892695a35d4331054dc58ac3c4ff7e73
mandu <jys0972@gmail.com>
2024-06-24 19:05:53 +0900

    refact Enemy.cs
    change EnemyAction class

--- Assets/JYS/Script/Enemy.cs
+++ Assets/JYS/Script/Enemy.cs
@@ -2,74 +2,73 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Unity.VisualScripting;
 
 namespace Enemy
 {
-    public enum ActionType
+
+    public abstract class EnemyAction
     {
-        ATTACK=0, MOVE=1, BLOCK=2
+        protected Enemy enemy;
+        protected EnemyAction(Enemy enemy)
+        {
+            this.enemy = enemy;
+        }
+        public abstract void PlayAction(float distanceToPlayer);
+
     }
 
-    public class Enemy : MonoBehaviour
+    public class EnemyAttackAction : EnemyAction
     {
 
-        
-        public abstract class EnemyAction
+        public EnemyAttackAction(Enemy enemy) : base(enemy) { }
+
+        public override void PlayAction(float distanceToPlayer)
         {
-            protected Enemy enemy;
-            protected EnemyAction(Enemy enemy)
-            {
-                this.enemy = enemy;
-            }
-            public abstract void Action(float distanceToPlayer);
-
+            enemy.Attack();
         }
+    }
 
-        public class EnemyAttackAction : EnemyAction
-        {
+    public class EnemyMoveAction : EnemyAction
+    {
 
-            public EnemyAttackAction(Enemy enemy):base(enemy) {}
-
-            public override void Action(float distanceToPlayer)
-            {
-                enemy.Attack();
-            }
-        }
-
-        public class EnemyMoveAction : EnemyAction
+        public EnemyMoveAction(Enemy enemy) : base(enemy) { }
+        public override void PlayAction(float distanceToPlayer)
         {
 
-            public EnemyMoveAction(Enemy enemy) : base(enemy) {}
-            public override void Action(float distanceToPlayer)
+            float tempMoveDistance;
+
+            if (distanceToPlayer > enemy.moveDistance + enemy.attackRange)
             {
-
-                float tempMoveDistance;
-
-                if (distanceToPlayer > enemy.moveDistance + enemy.attackRange)
-                {
-                    tempMoveDistance = enemy.moveDistance;
-                }
-                else
-                {
-                    tempMoveDistance = distanceToPlayer - enemy.attackRange;
-                }
-
-                enemy.StartCoroutine(enemy.MoveDistance(tempMoveDistance));
+                tempMoveDistance = enemy.moveDistance;
             }
-        }
+            else
+            {
+                tempMoveDistance = distanceToPlayer - enemy.attackRange;
+            }
 
-        public class EnemyBlockAction : EnemyAction
+            enemy.StartCoroutine(enemy.MoveDistance(tempMoveDistance));
+        }
+    }
+
+    public class EnemyBlockAction : EnemyAction
+    {
+        public EnemyBlockAction(Enemy enemy) : base(enemy) { }
+        public override void PlayAction(float distanceToPlayer)
         {
-            public EnemyBlockAction(Enemy enemy):base(enemy){}
-            public override void Action(float distanceToPlayer)
-            {
-                enemy.shield += 5;
-                enemy.UpdateIndicator();
-            }
+            enemy.shield += 5;
+            enemy.UpdateIndicator();
         }
+    }
+
+    public enum ActionType
+    {
+        ATTACK = 0, MOVE = 1, BLOCK = 2
+    }
 
 
-
+    public class Enemy : MonoBehaviour
+    { 
         protected Animator animator;
 
         protected TMP_Text hpText;
@@ -78,11 +77,11 @@ namespace Enemy
         [SerializeField] protected int id;
         protected int hp = 20;
         protected int maxHp = 20;
-        protected float moveDistance = 5;
+        public float moveDistance = 5;
 
-        protected int shield = 0;
+        public int shield = 0;
 
-        protected float attackRange = 3;
+        public float attackRange = 3;
 
         private Vector3 tempVector3 = new Vector3();
         float turnTime = 2;
@@ -127,7 +126,7 @@ namespace Enemy
             }
         }
 
-        private void UpdateIndicator()
+        public void UpdateIndicator()
         {
             hpText.SetText(hp.ToString());
             shieldText.SetText(shield.ToString());
@@ -138,11 +137,11 @@ namespace Enemy
         public void PlayTurnAction(float distanceToPlayer)
         {
             print((int)MakeActionDecision(distanceToPlayer));
-            enemyActions[(int) MakeActionDecision(distanceToPlayer)].Action(distanceToPlayer);
+            enemyActions[(int) MakeActionDecision(distanceToPlayer)].PlayAction(distanceToPlayer);
         }
 
 
-        IEnumerator MoveDistance(float distance)
+        public IEnumerator MoveDistance(float distance)
         {
             float moveSpeed = moveDistance / turnTime;
             float movedDistance = 0;
@@ -182,7 +181,7 @@ namespace Enemy
             transform.localScale = tempVector3;
         }
 
-        protected void Move(int direction, float moveStep) {
+        public void Move(int direction, float moveStep) {
             FaceToDirection(direction);
             tempVector3 = transform.position;
             tempVector3.x = tempVector3.x + moveStep * direction;
@@ -195,7 +194,7 @@ namespace Enemy
             animator.SetBool("isMoving", false);
         }
 
-        virtual protected void Attack()
+        virtual public void Attack()
         {
             animator.SetTrigger("Attack");
         }
