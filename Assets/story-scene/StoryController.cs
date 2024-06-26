41914546b4052be4490e8a24d7c2f7e69690bab4
mandu <jys0972@gmail.com>
2024-06-26 10:59:38 +0900

    refact(StoryController.cs) :
    scriptable Object에서 받아와서 출력하도록, background까지 적용할 수 있도록 추가

--- Assets/story-scene/StoryController.cs
+++ Assets/story-scene/StoryController.cs
@@ -1,39 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StoryController : MonoBehaviour
 {
 
     public ChatWindowController chatWindowController;
+    [SerializeField] ChatWindowScriptContainer scriptContainer;
 
-    List<string> storyDialog = new List<string>();
+    [SerializeField] List<GameObject> backgorunds = new List<GameObject>();
 
 
     void Start()
     {
-        storyDialog.Add("워드는 아름다운 시골 마을에서 자란 밝고 활기찬 소년이었다.");
-        storyDialog.Add("그의 맑은 목소리는 언제나 마을 사람들에게 즐거움을 선사했고, 그의 따뜻한 마음은 많은 사람들에게 사랑받았다.");
-        storyDialog.Add("하지만 평화로운 마을에 어느 날 갑자기 어둠이 드리워졌다.");
+        InitBackground();
         StartCoroutine(StoryTelling());
     }
 
-    private void Update()
+    private void InitBackground()
     {
-        //if (Input.GetKeyDown(KeyCode.Space))
-        //{
-        //    chatWindowController.UpdateChatStream("나레이터", "평화로운 어느날.");
-        //}
-    }
-
-    IEnumerator StoryTelling()
-    {
-        for (int i = 0; i < storyDialog.Count; i++)
+        backgorunds[0].SetActive(true);
+        for (int i = 1; i < backgorunds.Count; i++)
         {
-            yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
-            chatWindowController.UpdateChatStream("나레이터", storyDialog[i]);
-            yield return new WaitForSeconds(1);
+            backgorunds[i].SetActive(false);
         }
     }
 
+    private void SwitchBackground(int id)
+    {
+        for (int i = 0; i < backgorunds.Count; i++)
+        {
+            if (i == id)
+            {
+                backgorunds[i].SetActive(true);
+            } else
+                backgorunds[i].SetActive(false);
+        }
+    }
+
+    
+    IEnumerator StoryTelling()
+    {
+        for (int i = 0; i < scriptContainer.GetScriptNum(); i++)
+        {
+            SwitchBackground(scriptContainer.GetScriptData(i).background);
+            chatWindowController.UpdateChatStream(scriptContainer.GetScriptData(i).name, scriptContainer.GetScriptData(i).text);
+
+            yield return new WaitForSeconds(scriptContainer.GetScriptData(i).text.Length * 0.1f);
+            yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
+        }
+
+        LoadTitleScene();
+    }
+
+    private void LoadTitleScene()
+    {
+        SceneManager.LoadScene("TitleScene");
+    }
 }
