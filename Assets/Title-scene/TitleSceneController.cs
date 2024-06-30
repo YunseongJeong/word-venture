ba290aff943cf8460d7a505481cd4841020f3702
mandu <jys0972@gmail.com>
2024-06-30 20:03:14 +0900

    Add SaveLoad 기능 추가

--- Assets/Title-scene/TitleSceneController.cs
+++ Assets/Title-scene/TitleSceneController.cs
@@ -5,8 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class TitleSceneManager : MonoBehaviour
 {
+    SaveLoadController saveLoadController;
+
+
+    private void Start()
+    {
+        saveLoadController = GameObject.Find("SaveLoadController").GetComponent<SaveLoadController>();
+    }
+
     public void LoadStoryScene()
     {
-        SceneManager.LoadScene("StoryScene");
+        if (saveLoadController.LoadPlayData() == -1)
+        {
+            SceneManager.LoadScene("StoryScene");
+            Map_scene.MapMove.StagePosition = 0;
+            saveLoadController.SavePlayData();
+        } else
+        {
+            SceneManager.LoadScene("Map_scene");
+        }
+
+        
     }
 }
