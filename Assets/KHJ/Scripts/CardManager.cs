ec91140254aa6b1bbe9c23dc401c33376981aebd
191012 <kimhj191012@naver.com>
2024-07-03 14:36:47 +0900

    change stat of WordSO

--- Assets/KHJ/Scripts/CardManager.cs
+++ Assets/KHJ/Scripts/CardManager.cs
@@ -167,44 +167,54 @@ namespace Deck_Manage {
             switch(Map_scene.MapMove.StagePosition)
             {
                 case 0:
+                    wordSO.words[0].percent = 1;
                     wordSO.words[1].percent = 0;
                     wordSO.words[2].percent = 0;
+                    wordSO.words[3].percent = 1;
                     wordSO.words[4].percent = 0;
                     wordSO.words[5].percent = 0;
                     wordSO.words[6].percent = 0;
                     wordSO.words[7].percent = 0;
                     break;
                 case 1:
-                    wordSO.words[1].percent = 5;
-                    wordSO.words[2].percent = 5;
+                    wordSO.words[0].percent = 1;
+                    wordSO.words[1].percent = 1;
+                    wordSO.words[2].percent = 1;
+                    wordSO.words[3].percent = 3;
                     wordSO.words[4].percent = 0;
                     wordSO.words[5].percent = 0;
                     wordSO.words[6].percent = 0;
                     wordSO.words[7].percent = 0;
                     break;
                 case 2:
-                    wordSO.words[1].percent = 5;
-                    wordSO.words[2].percent = 5;
-                    wordSO.words[4].percent = 10;
+                    wordSO.words[0].percent = 2;
+                    wordSO.words[1].percent = 2;
+                    wordSO.words[2].percent = 2;
+                    wordSO.words[3].percent = 3;
+                    wordSO.words[4].percent = 3;
                     wordSO.words[5].percent = 0;
                     wordSO.words[6].percent = 0;
                     wordSO.words[7].percent = 0;
                     break;
                 case 3:
-                    wordSO.words[1].percent = 5;
-                    wordSO.words[2].percent = 5;
-                    wordSO.words[4].percent = 10;
-                    wordSO.words[5].percent = 10;
-                    wordSO.words[6].percent = 10;
+                    wordSO.words[0].percent = 4;
+                    wordSO.words[1].percent = 4;
+                    wordSO.words[2].percent = 4;
+                    wordSO.words[3].percent = 3;
+                    wordSO.words[4].percent = 3;
+                    wordSO.words[5].percent = 3;
+                    wordSO.words[6].percent = 3;
                     wordSO.words[7].percent = 0;
                     break;
                 case 4:
+                    wordSO.words[0].percent = 5;
                     wordSO.words[1].percent = 5;
                     wordSO.words[2].percent = 5;
-                    wordSO.words[4].percent = 10;
-                    wordSO.words[5].percent = 10;
-                    wordSO.words[6].percent = 10;
-                    wordSO.words[7].percent = 10;
+                    wordSO.words[3].percent = 3;
+                    wordSO.words[4].percent = 3;
+                    wordSO.words[5].percent = 3;
+                    wordSO.words[6].percent = 3;
+                    wordSO.words[7].percent = 3;
                     break;
                 default:
                     break;
